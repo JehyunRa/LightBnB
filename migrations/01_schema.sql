3a09f5e7064564a958115e0377767fb15e5b7a97
JehyunRa <jra4862@gmail.com>
2020-02-04 03:23:55 +0000

    lightbnb first file: migrations 01_schema.sql

---
+++ migrations/01_schema.sql
@@ -0,0 +1,66 @@
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  const_per_night REAL,
+  street VARCHAR(255),
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(225),
+  post_code VARCHAR(10),
+  active BOOLEAN
+);
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  start_date DATE,
+  end_date DATE,
+  cost_per_night REAL
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating INTEGER,
+  message VARCHAR(225)
+);
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating INTEGER,
+  message VARCHAR(225)
+);
