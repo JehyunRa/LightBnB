b91480f9d8de9c44eedf1f7b993bfef29985835b
JehyunRa <jra4862@gmail.com>
2020-02-05 06:40:15 +0000

    linked getAllReservations to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -84,7 +84,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [`${guest_id}`, `${limit}`];
+
+  return pool.query(`
+  SELECT *
+  FROM properties
+  JOIN reservations ON properties.id = reservations.guest_id
+  WHERE reservations.guest_id = $1
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, values)
+  .then(res => {
+    return(res.rows);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
